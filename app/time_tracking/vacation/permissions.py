77549cb755fdbf3812be5c98658434c910510365
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2020-08-09 20:18:36 +0300

    Added Event related constrains and more validation on the vacation date
    Signed-off-by: Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>

--- app/time_tracking/vacation/permissions.py
+++ app/time_tracking/vacation/permissions.py
@@ -1,11 +1,16 @@
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission
 
 
-class IsOwner(permissions.BasePermission):
+class IsOwner(BasePermission):
     """
     Custom permission to only allow owners of an object to edit it.
     """
 
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated)
+
     def has_object_permission(self, request, view, obj):
-        # Write permissions are only allowed to the owner of the snippet.
+        """
+        Write permissions are only allowed to the owner of the vacation.
+        """
         return obj.owner == request.user
