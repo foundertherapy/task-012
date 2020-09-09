3bdadd846f06b675e05e62ede150d4c116188033
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2020-09-10 02:20:47 +0300

    added pagination
    Signed-off-by: Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>

--- app/time_tracking/settings.py
+++ app/time_tracking/settings.py
@@ -139,7 +139,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 15,
 }
 
 CACHES = {
